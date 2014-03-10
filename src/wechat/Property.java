40d38f66527d2cb471aa61c3ed61c0222647fbd0
wrh1986 <dndx61003127@gmail.com>
2014-03-10 23:39:58 +0800

    wechat support platform develop

---
+++ src/wechat/Property.java
@@ -0,0 +1,11 @@
+package wechat;
+
+/**
+ * Created by wangronghua on 14-3-10.
+ */
+public class Property {
+
+  public static String APP_ID = "APPID";
+  public static String APP_SECRET = "APP_SECRET";
+  public static String APP_TOKEN = "wangronghua";
+}
