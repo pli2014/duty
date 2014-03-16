17089445dc1f48a49f7b3c0b66d725cccfe07194
pli2014 <lilimin2014>
2014-03-16 12:06:47 +0800

    resolve conflict code.

--- src/actions/UserServiceAction.java
+++ src/actions/UserServiceAction.java
@@ -16,112 +16,112 @@ import java.util.Map;
  * Created by wangronghua on 14-3-15.
  */
 public class UserServiceAction extends BaseAction {
-  List<UserServiceBean> userServices = null;
+    List<UserServiceBean> userServices = null;
 
-  List<ServicePlaceBean> servicePlaces = null;
-  UserServiceBusiness userServiceBus = (UserServiceBusiness) SingleBusinessPoolManager.getBusObj(BusTieConstant.BUS_CPATH_USERSERVICE);
-  private String userId;
-  private String servicePlaceId;
-  private long dayHours;
-  private long monthHours;
-  private long yearHours;
-  private long totalHours;
+    List<ServicePlaceBean> servicePlaces = null;
+    UserServiceBusiness userServiceBus = (UserServiceBusiness) SingleBusinessPoolManager.getBusObj(BusTieConstant.BUS_CPATH_USERSERVICE);
+    private String userId;
+    private String servicePlaceId;
+    private long dayHours;
+    private long monthHours;
+    private long yearHours;
+    private long totalHours;
 
-  public String getList(){
-    UserBean user = (UserBean)getSession().getAttribute(UserAction.LOGIN_USER_SESSION_ID);
-    if(null != user){
-      userServices = (List<UserServiceBean>)userServiceBus.getOrderedLeavesByUserId(user.getId(), 10).getResponseData();
+    public String getList(){
+        UserBean user = (UserBean)getSession().getAttribute(UserAction.LOGIN_USER_SESSION_ID);
+        if(null != user){
+            userServices = (List<UserServiceBean>)userServiceBus.getOrderedLeavesByUserId(user.getId(), 10).getResponseData();
+        }
+        return SUCCESS;
     }
-    return SUCCESS;
-  }
 
-  public String checkIn(){
-    UserBean user = (UserBean)getSession().getAttribute(UserAction.LOGIN_USER_SESSION_ID);
-    servicePlaces = (List<ServicePlaceBean>)userServiceBus.getAvailableServicePlaces(user.getId()).getResponseData();
-    return SUCCESS;
-  }
-
-  public String checkInSubmit(){
-    UserBean user = (UserBean)getSession().getAttribute(UserAction.LOGIN_USER_SESSION_ID);
-    userServiceBus.checkIn(user.getId(), servicePlaceId);
-    return SUCCESS;
-  }
-
-  public String checkOut() throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-    UserBean user = (UserBean)getSession().getAttribute(UserAction.LOGIN_USER_SESSION_ID);
-    userServiceBus.checkOut(user.getId());
-    return SUCCESS;
-  }
-
-  public String getMyTimeReport() {
-    UserBean user = (UserBean)getSession().getAttribute(UserAction.LOGIN_USER_SESSION_ID);
-    List<UserServiceBean> beanList = (List<UserServiceBean>)userServiceBus.getLeavesByUserId(user.getId()).getResponseData();
-    Map<String, Map> resultMap = userServiceBus.statisticTime(beanList);
-    Map result = resultMap.get(user.getId());
-    if(null != result){
-      dayHours = (long)result.get(Calendar.DAY_OF_MONTH) / 3600000 ;
-      monthHours = (long)result.get(Calendar.MONTH) / 3600000 ;
-      yearHours = (long)result.get(Calendar.YEAR) / 3600000 ;
-      totalHours = (long)result.get(Calendar.ALL_STYLES) / 3600000 ;
+    public String checkIn(){
+        UserBean user = (UserBean)getSession().getAttribute(UserAction.LOGIN_USER_SESSION_ID);
+        servicePlaces = (List<ServicePlaceBean>)userServiceBus.getAvailableServicePlaces(user.getId()).getResponseData();
+        return SUCCESS;
     }
-    return SUCCESS;
-  }
 
-  public List<UserServiceBean> getUserServices() {
-    return userServices;
-  }
+    public String checkInSubmit(){
+        UserBean user = (UserBean)getSession().getAttribute(UserAction.LOGIN_USER_SESSION_ID);
+        userServiceBus.checkIn(user.getId(), servicePlaceId);
+        return SUCCESS;
+    }
 
-  public void setUserServices(List<UserServiceBean> userServices) {
-    this.userServices = userServices;
-  }
+    public String checkOut() throws IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+        UserBean user = (UserBean)getSession().getAttribute(UserAction.LOGIN_USER_SESSION_ID);
+        userServiceBus.checkOut(user.getId());
+        return SUCCESS;
+    }
 
-  public UserServiceBusiness getUserServiceBus() {
-    return userServiceBus;
-  }
+    public String getMyTimeReport() {
+        UserBean user = (UserBean)getSession().getAttribute(UserAction.LOGIN_USER_SESSION_ID);
+        List<UserServiceBean> beanList = (List<UserServiceBean>)userServiceBus.getLeavesByUserId(user.getId()).getResponseData();
+        Map<String, Map> resultMap = userServiceBus.statisticTime(beanList);
+        Map result = resultMap.get(user.getId());
+        if(null != result){
+            dayHours = (long)result.get(Calendar.DAY_OF_MONTH) / 3600000 ;
+            monthHours = (long)result.get(Calendar.MONTH) / 3600000 ;
+            yearHours = (long)result.get(Calendar.YEAR) / 3600000 ;
+            totalHours = (long)result.get(Calendar.ALL_STYLES) / 3600000 ;
+        }
+        return SUCCESS;
+    }
 
-  public void setUserServiceBus(UserServiceBusiness userServiceBus) {
-    this.userServiceBus = userServiceBus;
-  }
+    public List<UserServiceBean> getUserServices() {
+        return userServices;
+    }
 
-  public String getUserId() {
-    return userId;
-  }
+    public void setUserServices(List<UserServiceBean> userServices) {
+        this.userServices = userServices;
+    }
 
-  public void setUserId(String userId) {
-    this.userId = userId;
-  }
+    public UserServiceBusiness getUserServiceBus() {
+        return userServiceBus;
+    }
 
-  public String getServicePlaceId() {
-    return servicePlaceId;
-  }
+    public void setUserServiceBus(UserServiceBusiness userServiceBus) {
+        this.userServiceBus = userServiceBus;
+    }
 
-  public void setServicePlaceId(String servicePlaceId) {
-    this.servicePlaceId = servicePlaceId;
-  }
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getServicePlaceId() {
+        return servicePlaceId;
+    }
+
+    public void setServicePlaceId(String servicePlaceId) {
+        this.servicePlaceId = servicePlaceId;
+    }
 
 
-  public List<ServicePlaceBean> getServicePlaces() {
-    return servicePlaces;
-  }
+    public List<ServicePlaceBean> getServicePlaces() {
+        return servicePlaces;
+    }
 
-  public void setServicePlaces(List<ServicePlaceBean> servicePlaces) {
-    this.servicePlaces = servicePlaces;
-  }
+    public void setServicePlaces(List<ServicePlaceBean> servicePlaces) {
+        this.servicePlaces = servicePlaces;
+    }
 
 
-  public long getDayHours() {
-    return dayHours;
-  }
+    public long getDayHours() {
+        return dayHours;
+    }
 
-  public long getMonthHours() {
-    return monthHours;
-  }
+    public long getMonthHours() {
+        return monthHours;
+    }
 
-  public long getYearHours() {
-    return yearHours;
-  }
+    public long getYearHours() {
+        return yearHours;
+    }
 
-  public long getTotalHours() {
-    return totalHours;
-  }
+    public long getTotalHours() {
+        return totalHours;
+    }
 }
